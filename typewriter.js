1a1be8dc54f3939805720b0808e4ccf86b1cb9fa
Elyse <elyse-groenendaal@outlook.com>
2022-04-14 11:42:07 +0900

    added typewriter effect JS

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+//import Typewriter from "typewriter-effect/dist/core";
+
+var app = document.getElementById("app");
+
+var typewriter = new Typewriter(app, {
+  loop: false,
+  delay: 90,
+});
+
+typewriter.typeString("Hallo, ik ben Elyse.").start();
+document.querySelectorAll(".Typewriter__cursor")[0].style.display = "none";
